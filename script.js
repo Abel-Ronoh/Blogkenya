f4f5ba74bf32845f5fe0847903c0c3744f438ade
Abel Ronoh <114028959+Abel-Ronoh@users.noreply.github.com>
2023-02-11 00:50:19 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,85 @@
+var img1=document.querySelector(".img1");
+var img2=document.querySelector(".img2");
+var img3=document.querySelector(".img3");
+var img4=document.querySelector(".img4");
+var img5=document.querySelector(".img5");
+
+var image1=document.getElementById("img1");
+var image2=document.getElementById("img2");
+var image3=document.getElementById("img3");
+var image4=document.getElementById("img4");
+var image5=document.getElementById("img5");
+
+
+image5.addEventListener("mouseover",()=>{
+    image5.style.cursor="pointer";
+    img5.style.left="0";
+});
+image5.addEventListener("mouseout",()=>{
+   
+    img5.style.left="-220px";
+});
+image4.addEventListener("mouseover",()=>{
+    image4.style.cursor="pointer";
+    img4.style.left="0";
+});
+image4.addEventListener("mouseout",()=>{
+   
+    img4.style.left="-220px";
+});
+image3.addEventListener("mouseover",()=>{
+    image3.style.cursor="pointer";
+    img3.style.left="0";
+});
+image3.addEventListener("mouseout",()=>{
+   
+    img3.style.left="-220px";
+});
+image2.addEventListener("mouseover",()=>{
+    image2.style.cursor="pointer";
+    img2.style.left="0";
+});
+image2.addEventListener("mouseout",()=>{
+   
+    img2.style.left="-220px";
+});
+image1.addEventListener("mouseover",()=>{
+    image1.style.cursor="pointer";
+    img1.style.left="0";
+});
+image1.addEventListener("mouseout",()=>{
+    img1.style.left="-220px";
+});
+
+
+
+img5.addEventListener("mouseover",()=>{
+    img5.style.left="0";
+});
+img5.addEventListener("mouseout",()=>{
+    img5.style.left="-220px";
+});
+img4.addEventListener("mouseover",()=>{
+    img4.style.left="0";
+});
+img4.addEventListener("mouseout",()=>{
+    img4.style.left="-220px";
+});
+img3.addEventListener("mouseover",()=>{
+    img3.style.left="0";
+});
+img3.addEventListener("mouseout",()=>{
+    img3.style.left="-220px";
+});
+img2.addEventListener("mouseover",()=>{
+    img2.style.left="0";
+});
+img2.addEventListener("mouseout",()=>{
+    img2.style.left="-220px";
+});
+img1.addEventListener("mouseover",()=>{
+    img1.style.left="0";
+});
+img1.addEventListener("mouseout",()=>{
+    img1.style.left="-220px";
+});
